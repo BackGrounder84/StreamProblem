5ba5a6f41b55bcb728aab08dbdd096e30790a6d1
Sebastian Schickentanz <Sebastian.Schickentanz@mt-ag.com>
2022-05-09 08:57:02 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # StreamProblem
 Demo für das Stream Problem
+
+# Url für den Change
+https://docs.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/partial-byte-reads-in-streams
+
+# Anleitung
+Um die Änderung zu sehen muss Framework Version in Projekt zwischen 5 und 6 geändert werden
+5 -> ProcessZipNet5 und ProcessZipNet6 Produzieren den selben Output
+6 -> ProcessZipNet5 beinhaltet Nullen, ProcessZipNet6 hat diese nicht
+
---
+++ StreamProblem/StreamProblem/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace StreamProblem
+{
+    internal static class Program
+    {
+        //https://docs.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/partial-byte-reads-in-streams
+        //Um die Änderung zu sehen Framework Version zwischen 5 <-> 6 ändern
+
+        public static void Main(string[] args)
+        {
+            var pdfData = File.ReadAllBytes("OcrInvoice.pdf");
+            PrintData(pdfData, "original.txt");
+
+
+            Console.ReadLine();
+            Console.Clear();
+
+            var zipFileData = File.ReadAllBytes("Data.zip");
+            var dataArrayOld = ProcessZipNet5(zipFileData);
+
+            PrintData(dataArrayOld, "net5.txt");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            var dataArrayNew = ProcessZipNet6(zipFileData);
+
+            PrintData(dataArrayNew, "net6.txt");
+
+            Console.ReadLine();
+        }
+
+        private static void PrintData(byte[] pdfData, string fileName)
+        {
+            var sb = new StringBuilder();
+            foreach (var b in pdfData)
+            {
+                Console.Write(b);
+            }
+            File.WriteAllText(fileName, sb.ToString());
+        }
+
+        private static byte[] ProcessZipNet5(byte[] fileData)
+        {
+            using var stream = new MemoryStream(fileData, false);
+            using var zipArchive = new ZipArchive(stream);
+
+
+            var entry = zipArchive.Entries.First();
+            var data = new byte[entry.Length];
+            using var entryStream = entry.Open();
+            entryStream.Read(data, 0, data.Length);
+
+            return data;
+
+        }
+
+        private static byte[] ProcessZipNet6(byte[] fileData)
+        {
+            using var stream = new MemoryStream(fileData, false);
+            using var zipArchive = new ZipArchive(stream);
+
+
+            var entry = zipArchive.Entries.First();
+            Span<byte> buffer = new byte[entry.Length];
+
+            int totalRead = 0;
+            using var pdfStream = entry.Open();
+
+            while (totalRead < buffer.Length)
+            {
+                int bytesRead = pdfStream.Read(buffer.Slice(totalRead));
+
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                totalRead += bytesRead;
+            }
+
+            return buffer.ToArray();
+
+        }
+    }
+}
